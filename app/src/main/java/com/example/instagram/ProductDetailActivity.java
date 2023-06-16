1d6a914206c7f704a59497fce2d8676a6dcc7f0c
tainguyen58dev <tainguyen58dev@gmail.com>
2023-06-16 13:48:03 +0700

    demo - get img from online

--- app/src/main/java/com/example/instagram/ProductDetailActivity.java
+++ app/src/main/java/com/example/instagram/ProductDetailActivity.java
@@ -3,6 +3,9 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+
+        ImageView imageProductDetail = findViewById(R.id.imageView7);
+
+        String imageUrl = "https://i.pinimg.com/564x/c9/a2/7b/c9a27b13884779ab61cc23756ea2b05f.jpg";
+        Picasso.get().load(imageUrl).into(imageProductDetail);
+//        Picasso.get()
+//                .load(imageUrl)
+//                .fit()
+//                .placeholder(R.drawable.placeholder_image) // Replace with your placeholder image resource
+//                .into(imageProductDetail);
     }
 }
