8f5c687e38d7fd1d2f7a25fc0f3b36c5a9979b7a
THUC NGUYEN <ntthuc321@gmail.com>
2023-07-09 21:50:11 +0700

    fix

--- app/src/main/java/com/example/instagram/ProfileActivity.java
+++ app/src/main/java/com/example/instagram/ProfileActivity.java
@@ -29,7 +29,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     private Button btnReturn, btnLogout, btnEdit;
     private TextView txtName, txtEmail, txtAddress, txtPhone;
-    private String customerId;
+    private int customerId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +41,15 @@ public class ProfileActivity extends AppCompatActivity {
         btnEdit = findViewById(R.id.edit_profile);
         btnLogout = findViewById(R.id.logout_button);
 
-        customerId = "1";
-//        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", Context.MODE_PRIVATE);
-//        customerId = sharedPreferences.getString("customerId","0");
-//
-//        if (customerId == "0") {
-//            Intent intent = new Intent(ProfileActivity.this, Login.class);
-//            startActivity(intent);
-//            finish();
-//            return;
-//        }
+        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", Context.MODE_PRIVATE);
+        customerId = sharedPreferences.getInt("customerId",0);
+
+        if (customerId == 0) {
+            Intent intent = new Intent(ProfileActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://6482d5d3f2e76ae1b95b92a6.mockapi.io/")
@@ -58,7 +57,7 @@ public class ProfileActivity extends AppCompatActivity {
                 .build();
         ApiService apiService = retrofit.create(ApiService.class);
 
-        Call<Customer> call = apiService.getCustomerById(customerId);
+        Call<Customer> call = apiService.getCustomerById(String.valueOf(customerId));
 
         Log.d("API Request", "URL: " + call.request().url());
 
@@ -67,6 +66,7 @@ public class ProfileActivity extends AppCompatActivity {
             public void onResponse(Call<Customer> call, Response<Customer> response) {
                 if (response.isSuccessful()) {
                     Customer customer = response.body();
+                    Log.d("A", customer.getName());
                     if (customer != null) {
                         BindingData(customer);
                     }
